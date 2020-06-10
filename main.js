744d7786755ee35481b6a8be2dd61b27f9e37817
nakanoh <murry.xxx.7@gmail.com>
2020-06-10 18:20:58 +0900

    ~MarkdownをHTMLに変換する

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const program = require("commander");
 const fs = require("fs");
+const marked = require("marked");
 
+program.option("--gfm", "GFMを有効にする")
 program.parse(process.argv)
 const fp = program.args[0];
+
+const cliOptions = {
+    gfm: false,
+    ...program.opts()
+}
 
 fs.readFile(fp, { encoding: "utf8" }, (err, file)=>{
     if(err){
@@ -10,6 +17,6 @@ fs.readFile(fp, { encoding: "utf8" }, (err, file)=>{
         process.exit(1);
         return;
     }
-    console.log(file);
+    const html = marked(file, {gfm: cliOptions.gfm});
+    console.log(html);
 });
-// console.log(fp);
